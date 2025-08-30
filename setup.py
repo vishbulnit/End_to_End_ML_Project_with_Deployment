9dc133046998bc3663ef99f4566c99f397cb2735
Vishnukumar Vishwakarma <vishwakarma91@gmail.com>
2025-08-30 16:12:32 +0530

    code commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hypen_e_dot = '-e .'
+
+## use this and modify when dynamically list to be passed to setup- install_requires
+def get_requirements(file_path:str)-> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    
+    with open(file_path) as file_obj:
+        requirements = file_obj.readline()
+        requirements = [req.replace('\n',"") for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+    
+    return requirements
+
+setup (
+name='End to End ML Project with Deployment',
+version='0.0.1',
+author='Vishnukumar',
+author_email='vishwakarmavm91@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','seaborn','numpy'],
+##install_requires=get_requirements('requirements.txt'),
+)
